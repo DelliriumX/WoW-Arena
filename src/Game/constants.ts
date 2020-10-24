6c9293a037dedf822c3617350e8d1166f598e0ae
Mitar Skoro <mitarskoro@gmail.com>
2020-10-24 22:47:32 +0200

    constants added

---
+++ src/Game/constants.ts
@@ -0,0 +1,34 @@
+export const MAX_PLAYERS = 12
+export const GAME_LOOP_TICK_INTERVAL = 0.0312500
+
+//=============================================
+//          ANIMATION DATA CONSTANTS
+//=============================================
+export const UNIT_TYPE_FIREMAGE = 'H002'
+export const FIREMAGE_SPELL_INDEX = 20
+export const FIREMAGE_SPELL_READY_INDEX = 11
+export const FIREMAGE_SPELL_CHANNEL_INDEX = 23
+export const FIREMAGE_SPELL_THROW_INDEX = 10
+export const FIREMAGE_SPELL_READY_THROW_INDEX = 9
+export const FIREMAGE_SPELL_CHANNEL_THROW_INDEX = 22
+export const FIREMAGE_SPELL_SLAM_INDEX = 21
+export const FIREMAGE_SPELL_ORIGIN_ANGLE = -13.0
+export const FIREMAGE_SPELL_ORIGIN_DISTANCE = 125.0
+export const FIREMAGE_SPELL_CAST_POINT = 0.55
+export const FIREMAGE_SPELL_THROW_CAST_POINT = 0.20
+export const FIREMAGE_SPELL_SLAM_CAST_POINT = 0.70
+export const FIREMAGE_CAST_BACKSWING = 0.20
+
+
+//=============================================
+//          SPELL DATA CONSTANTS
+//=============================================
+export const FROSTBOLT_CASTTIME = 2.30
+export const FROSTBOLT_POWER_COEF = 0.8571
+export const FROSTBOLT_DAMAGE_MIN = 799
+export const FROSTBOLT_DAMAGE_MAX = 861
+
+export const FROSTBOLT_ALT_CASTTIME = 1.5
+export const FROSTBOLT_ALT_POWER_COEF = 0.4286
+export const FROSTBOLT_ALT_DAMAGE_MIN = 15
+export const FROSTBOLT_ALT_DAMAGE_MAX = 20
